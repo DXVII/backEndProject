b12abbc07dd632dcd601e27aa093d7ab5ebd6352
DXVII <david.pham.d@gmail.com>
2022-12-27 10:27:26 +0700

    basic html definition and http status ode

--- index.js
+++ index.js
@@ -8,8 +8,21 @@ const url = require('url');
 // -- Server
 // create callback function server for every single request call
 const server = http.createServer((req, res) => {
-    const pathName = req.url
-    res.end(`Hello from the server!\nPath detected: ${pathName}`);
+    const pathName = req.url.toLowerCase();
+    let message = ""
+    if (pathName === "/" || pathName === "/overview"){
+        message = "<h1>Navigating to Overview</h1>";
+        res.writeHead(200);
+    } 
+    else if(pathName === "/product"){
+        message = "<h1>Navigating to Product</h1>";
+        res.writeHead(200);
+    } 
+    else {
+        message = "<h1>Page not found</h1>"
+        res.writeHead(404,{'Content-type':'text/html'});
+    }
+    res.end(`Hello from the server!\n  Path detected: ${pathName}\n${message}`);
 });
 
 server.listen(3000,'127.0.0.1',() => {
