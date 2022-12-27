20f2cffe47204cc87702510a152bcd519e742cff
DXVII <david.pham.d@gmail.com>
2022-12-27 10:06:10 +0700

    begin routing

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 // building a simple http server
+const fs = require('fs');
 const http = require('http');
+const url = require('url');
+
 // -- Files
 
 // -- Server
 // create callback function server for every single request call
 const server = http.createServer((req, res) => {
-    console.log(req);
-    res.end('Hello from the server')
+    const pathName = req.url
+    res.end(`Hello from the server!\nPath detected: ${pathName}`);
 });
 
 server.listen(3000,'127.0.0.1',() => {
