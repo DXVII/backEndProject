71882636e722d130048f14959f94d6075141a1eb
DXVII <david.pham.d@gmail.com>
2022-12-30 14:05:00 +0700

    simple webstie completed

--- index.js
+++ index.js
@@ -4,26 +4,43 @@ const http = require('http');
 const url = require('url');
 
 // -- Functions
-// import { fillCard } from "./src/helperfunctions/fillCards.js";
 
-const fillCard = (card, jsonInstance) => {
-    const cardVars = ['ID','IMAGE','PRODUCTNAME','QUANTITY','PRICE'];
-    const jsonVars = ['id','image','productName','quantity','price'];
-    let temp = card;
-    for (let i=0; i<cardVars.length; i++) {
-        let cardRegex = new RegExp(`{%${cardVars[i]}%}`,'g')
+
+const fillTemplate = (template, jsonInstance) => {
+    const templateVars = ['ID','PRODUCTNAME','IMAGE','FROM','NUTRIENTS','QUANTITY','PRICE','DESCRIPTION'];
+    const jsonVars = ['id','productName','image','from','nutrients','quantity','price','description'];
+    // fill all 
+    let temp = template;
+    for (let i=0; i<templateVars.length; i++) {
+        let templateRegex = new RegExp(`{%${templateVars[i]}%}`,'g')
         let jsonKey = jsonVars[i];
         let jsonValue = jsonInstance[jsonKey];
-        temp = temp.replace(cardRegex, jsonValue);
+        temp = temp.replace(templateRegex, jsonValue);
     }
-    if(!jsonInstance.organic){
-        temp = temp.replace('{%NOT_ORGANIC%}',"not-organic");
-        temp = temp.replace('card__detail--organic','card__detail--not-organic');
-        temp = temp.replace("Organic!","Not Organic!");
-    } else{
-        temp = temp.replace('{%NOT_ORGANIC%}',"organic");
-    } 
+    temp = fillOrganic(temp, jsonInstance.organic);
     return(temp)
+}
+
+const fillOrganic = (template, ifOrganic)=>{
+    let temp = template;
+    // organic 
+    if(ifOrganic){
+        temp = temp.replace('{%NOT_ORGANIC%}',"organic");
+        
+    } else {
+        temp = temp.replace('{%NOT_ORGANIC%}',"not-organic");
+        
+        // Not organic -- Card
+        if(template.includes('<figure class="card">')){
+            temp = temp.replace('card__detail--organic','card__detail--not-organic');
+            temp = temp.replace("Organic!","Not Organic!");
+        }
+        // Not Organic -- product
+        else {
+            temp = temp.replace("Organic","Not Organic");
+        }
+    }
+    return (temp)
 }
 
 // -- Files
@@ -33,7 +50,7 @@ const tempProduct = fs.readFileSync(`${__dirname}/src/templates/template-product
 const data = fs.readFileSync(`${__dirname}/src/dev-data/data.json`,'utf-8');
 const dataObject = JSON.parse(data);
 
-let cardsHTML = dataObject.map(el => fillCard(tempCard, el));
+let cardsHTML = dataObject.map(el => fillTemplate(tempCard, el));
 
 // -- Server
 const content_HTML = {'Content-type': 'text/html'};
@@ -41,8 +58,15 @@ const content_JSON = {'Content-type': 'application/json'};
 
 // Creates a callback function server for every single request call
 const server = http.createServer((req, res) => {
-    const pathName = req.url.toLowerCase();
-
+    const urlPath = req.url;
+    const urlHost = "http://"+req.headers.host;
+    const urlObj = new URL(urlPath, urlHost);
+    const pathName = urlObj.pathname
+    const searchParams = new URLSearchParams(urlObj.searchParams);
+    const paramMap = new Map(searchParams)
+    // for(const el of searchParams){
+    //     console.log(el);
+    // }
     // -- Overview Page
     if (pathName === "/" || pathName === "/overview"){
         let message = "<h1>Navigating to Overview</h1>";
@@ -51,14 +75,16 @@ const server = http.createServer((req, res) => {
         res.end(filledOverview);
     } 
 
-    else if (pathName === "/cards"){
-        res.writeHead(200);
-        res.end(cardsHTML.toString());
-    }
-
-    else if(pathName === "/product"){
+    else if(pathName === "/product/" || pathName === "/product"){
+        let queryID = parseInt(paramMap.get('id'));
+        try {
+            filledProduct = fillTemplate(tempProduct,dataObject[queryID]);
+        } catch(err){
+            filledProduct = tempOverview.replace(`{%PRODUCT_CARDS%}`,cardsHTML);
+        }
         res.writeHead(200, content_HTML);
-        res.end(tempProduct);
+        fs.writeFileSync(`${__dirname}/txt/filledProductID${queryID}.html`, filledProduct);
+        res.end(filledProduct);
     }
 
 
